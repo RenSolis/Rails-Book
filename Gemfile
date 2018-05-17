d9b6df7ca85250a43039eaef69c59e12a981557d
Renzo Solis <rensoliscastillo@gmail.com>
2018-05-17 01:30:07 -0500

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 gem 'faker', '~> 1.8', '>= 1.8.7'
 gem 'will_paginate', '~> 3.1', '>= 3.1.6'
 gem 'bootstrap-will_paginate', '~> 1.0'
+gem 'carrierwave', '~> 1.2', '>= 1.2.2'
+gem 'mini_magick', '~> 4.8'
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  get 'sessions/new'
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
@@ -17,5 +12,7 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
+
   root 'static_pages#home'
 end
